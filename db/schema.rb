29e0146612f7d1fcfc77bc3905db5449643ea58e
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-23 12:32:20 +0430

    Denylist added to the damn thing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_202115) do
+ActiveRecord::Schema.define(version: 2021_05_23_075813) do
+
+  create_table "jwt_denylists", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
