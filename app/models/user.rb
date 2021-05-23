aa73163b31ed9073a56a16ed3673d3fca3c538c1
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-23 12:28:25 +0430

    User bugs fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :jwt_authenticable 
-
-         jwt_revocation_strategy: JwtDenylist
+         :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 end
