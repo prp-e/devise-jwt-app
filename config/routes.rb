2cf985944f9484a15d5107c4a1ef10b2a1efcc5e
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-05-23 12:46:28 +0430

    New route /test added

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
     registrations: "registrations"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/test', to "test#show"
 end
